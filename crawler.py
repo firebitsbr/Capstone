3f25d9ffef828f857b27a0b05f445fe1efbfb2ba
Edward Mead <exm6939@rit.edu>
2016-03-16 15:32:23 -0400

    Added Crawler class, minor changes to result, added unit-test for crawler class.

---
+++ crawler.py
@@ -0,0 +1,137 @@
+# Crawler
+# Class / Template for all crawlers to inherit
+# Edward Mead
+from crawlerconfig import CrawlerConfig
+from result import Result
+
+class Crawler:
+    name = "Default Crawler"
+
+    def __init__(self, config):
+        self.config = config
+        self.result = Result(config)
+
+    # String
+    def __str__(self):
+        return self.name
+
+    # Initiate a crawl using a specific crawl config or the one used when initialized
+    def do_Crawl(self, config=None):
+        if (config == None):
+            config = self.config
+        print "Not implemented exception. Do not use this class directly. It should be inherited."
+        raise
+
+    # Get - Crawl Config object
+    def get_crawl_config(self):
+        return self.config
+
+    # Get - Crawl location
+    def get_crawl_location(self):
+        return self.config.get_location()
+
+    # Get - Crawl protocol
+    def get_crawl_protocol(self):
+        return self.config.get_protocol()
+
+    # Get - Crawl speed
+    def get_crawl_speed(self):
+        return self.config.get_speed()
+
+    # Get - Crawl max depth
+    def get_crawl_maxDepth(self):
+        return self.config.get_maxDepth()
+
+    # Get - Crawl name
+    def get_crawl_name(self):
+        return self.config.get_name()
+
+    # Get - Crawl depth
+    def get_crawl_depth(self):
+        return self.config.get_depth()
+
+    # Inc - Crawl Depth + 1
+    def inc_crawl_depth(self):
+        self.config.inc_depth()
+
+    # Set - Crawl speed
+    def set_crawl_speed(self, speed):
+        self.config.set_speed(speed)
+
+    # Set - Crawl max depth
+    def set_crawl_maxDepth(self, maxDepth):
+        self.config.set_maxDepth(maxDepth)
+
+    # Set - Crawl name
+    def set_crawl_name(self, name):
+        self.config.set_name(name)
+
+    # Set - Crawl depth
+    def set_crawl_depth(self, depth):
+        self.config.set_depth(depth)
+
+    # Get - Result object
+    def get_result(self):
+        return self.result
+
+    # Get - Result data hash
+    # Get the current hash of the data
+    def get_result_dataHash(self):
+        return self.result.get_dataHash()
+
+    # Get - Result source
+    def get_result_source(self):
+        return self.result.get_source()
+
+    # Set - Result source
+    def set_result_source(self, source):
+        self.result.set_source(source)
+
+    # Get - Result data
+    def get_result_data(self):
+        return self.result.get_data()
+
+    # Set - Result data
+    def set_result_data(self, data):
+        self.result.set_data(data)
+
+    # Add - Result data
+    def add_result_data(self, data):
+        self.result.add_data(data)
+
+    # Get - Result time start
+    def get_result_timeStart(self):
+        return self.result.get_timeStart()
+
+    # Set - Result time start
+    def set_result_timeStart(self, timeStart):
+        self.result.set_timeStart(timeStart)
+
+    # Get - Result time end
+    def get_result_timeEnd(self):
+        return self.result.get_timeEnd()
+
+    # Set - Result time end
+    def set_result_timeEnd(self, timeEnd):
+        self.result.set_timeEnd(timeEnd)
+
+    # Get - Result Crawler Configuration
+    def get_result_crawlerConfig(self):
+        return self.result.get_crawlerConfig()
+
+    # Set - Result Crawler Configuration
+    def set_result_crawlerConfig(self, crawlerConfig):
+        self.result.set_crawlerConfig(crawlerConfig)
+
+    # Get - Result Referrer
+    def get_result_referrer(self):
+        return self.result.get_referrer()
+
+    # Set - Result Referrer
+    def set_result_referrer(self, referrer):
+        self.result.set_referrer(referrer)
+
+    def send_result(self):
+        # @todo send results to ELK
+        print "Not implemented exception. To be added later."
+        raise
---
+++ result.py
@@ -0,0 +1,82 @@
+# Results Class
+# Used to store the results of a Crawl
+# Edward Mead
+import hashlib
+
+class Result:
+    def __init__(self, crawlerConfig, timeStart = None, timeEnd = None, source = None, referrer = None, data = ""):
+        self.crawlerConfig = crawlerConfig  # Crawl Configuration
+        self.timeStart = timeStart          # Time of Start (DateTime.Now when Crawl starts)
+        self.timeEnd = timeEnd              # Time of Completion (DateTime.Now after results are retrieved)
+        self.source = source                # Location
+        self.referrer = referrer            # Previous Location
+        self.data = data                    # Data from Location
+        self.dataHash = self.calc_hash(data)     # Hash of Data
+
+    # Calculate Hash of some data
+    def calc_hash(self, data):
+        if data != None and len(data) > 0:
+            h = hashlib.sha256()
+            h.update(data)
+            return h.digest()
+
+    # Get - Where the data was obtained
+    def get_source(self):
+        return self.source
+
+    # Set - Where the data was obtained
+    # ex: URL, IP, TOR onion address, etc
+    def set_source(self, source):
+        self.source = source
+
+    # Get - Data obtained from the crawl
+    def get_data(self):
+        return self.data
+
+    # Set - Data obtained from the crawl
+    # Hash is automatically updated
+    def set_data(self, data):
+        self.data = data
+        self.dataHash = self.calc_hash(data)
+
+    # Add - Data obtained from the crawl
+    # Hash is automatically updated
+    def add_data(self, data):
+        self.data += data
+        self.dataHash = self.calc_hash(data)
+
+    # Get - Hash of the Data obtained
+    def get_dataHash(self):
+        return self.dataHash
+
+    # Get - Start time of the crawl
+    def get_timeStart(self):
+        return self.timeStart
+
+    # Set - Start time of the crawl
+    def set_timeStart(self, timeStart):
+        self.timeStart = timeStart
+
+    # Get - End time of the crawl
+    def get_timeEnd(self):
+        return self.timeEnd
+
+    # Set - End time of the crawl
+    def set_timeEnd(self, timeEnd):
+        self.timeEnd = timeEnd
+
+    # Get - Crawler config used to obtain this data
+    def get_crawlerConfig(self):
+        return self.crawlerConfig
+
+    # Set - Crawler config used to obtain this data
+    def set_crawlerConfig(self, crawlerConfig):
+        self.crawlerConfig = crawlerConfig
+
+    # Get - Crawler config of the previous site
+    def get_referrer(self):
+        return self.referrer
+
+    # Set - Crawler config of the previous site
+    def set_referrer(self, referrer):
+        self.referrer = referrer
