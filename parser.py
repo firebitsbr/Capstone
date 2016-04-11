bc3a972fc432e13a8d638a8cfc1d275089a8fae5
robopt <exm6939@rit.edu>
2016-04-11 11:35:25 -0400

    push results to elasticsearch

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ import ssl
 import socket
 import socketserver
 import jsonpickle
+from es_result import es_result
 
 class parser(socketserver.BaseRequestHandler):
     cert_file="<insert pub cert file>"
@@ -10,6 +11,7 @@ class parser(socketserver.BaseRequestHandler):
 
     def __init__(self,search):
         self.search = search
+        es_result.init()
 
     def setup(self):
         self.context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
@@ -45,6 +47,32 @@ class parser(socketserver.BaseRequestHandler):
                 searchhits, regexhits = search.apply_terms(r.data)
                 print("Searchhits: " + str(searchhits))
                 print("Regexhits: " + str(regexhits))
+                esr = es_result()
+                esr.source = r.source
+                esr.referrer = r.referrer
+                esr.data = r.data
+                esr.dataHash = r.dataHash
+                if len(regexhits) > 0:
+                    esr.regex_hit = 1
+                    esr.regex_hits = "\n".join(regex_hits)
+                else:
+                    esr.regex_hit = 0
+                if len(searchhits) > 0:
+                    esr.searchterm_hit = 1
+                    esr.searchterm_hits = "\n".join(searchhits)
+                else:
+                    esr.searchterm_hit = 0
+                esr.timeStart = r.timeStart
+                esr.timeEnd = r.timeEnd
+                c = r.crawlerConfig
+                esr.config_name = c.name
+                esr.config_location = c.location
+                esr.config_protocol = c.protocol
+                esr.config_speed = c.speed
+                esr.config_depth = c.depth
+                esr.config_maxDepth = c.maxDepth
+                esr.config_options = c.options
+                esr.save()
             return True
         return False
 
