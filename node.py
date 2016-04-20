ae1ebd32a0307eada963030a9d78c16624846615
ajp7562 <ajp7562@rit.edu>
2016-04-20 15:23:40 -0400

    Adding Web/TOR crawler to master

---
+++ node.py
@@ -0,0 +1,22 @@
+class node:
+
+	def __init__(self, parent, url, currentDepth):
+		self.parent = parent
+		self.url = url
+		self.currentDepth = currentDepth
+		self.children = []
+
+	def get_url(self):
+		return self.url	
+
+	def get_parent(self):
+		return self.parent
+
+	def get_currentDepth(self):
+		return self.currentDepth
+
+	def set_children(self, children):
+		self.children = list(children)
+
+	def get_children(self):
+		return self.children
