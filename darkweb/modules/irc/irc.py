e75f62d32bfae9bd226ff2f62ad5cb7dab6de654
root <root@localhost.localdomain>
2016-05-08 21:16:21 -0400

    irc fixes

--- darkweb/modules/irc/irc.py
+++ darkweb/modules/irc/irc.py
@@ -15,48 +15,58 @@ from darkweb.modules.base.crawlerconfig import *
 
 class IRC(Crawler):
     def __init__(self, config, nickname="aaabbbcccddd", mins_interval="10", total_mins="60"):
-        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.config = config
         self.server = config.location
 	self.channels = []
+	self.num_channels = None
 	if "nickname" in config.options.keys():
 		self.nickname = config.options["nickname"]
 	else:
 		self.nickname = nickname
 	if "mins_interval" in config.options.keys():
-		self.mins_interval = config.options["mins_interval"]
+		self.mins_interval = int(config.options["mins_interval"])
 	else:
 		self.mins_interval = mins_interval
 	if "total_mins" in config.options.keys():
-		self.total_mins = config.options["total_mins"]
+		self.total_mins = int(config.options["total_mins"])
 	else:
 		self.total_mins = total_mins
 	if "channels" in config.options.keys():
 		for channel in config.options["channels"].split(","):
 			self.channels.append(channel)
+		self.num_channels = len(self.channels)
 		# self.channel = config.options["channel"]
-	else:
-		self.channel = channel
-
+	if self.num_channels == None:
+		if "num_channels" in config.options.keys():
+			self.num_channels = int(config.options['num_channels'])
+		else:
+			self.num_channels = 1
         # self.connect()
         # self.channels = self.getChannels()
 	# self.channels = []
         self.run(self.channels)
 
     def connect(self):
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("Connecting to server... %s" % self.server)
         self.irc.connect((self.server, 6667))
         self.irc.send("USER " + self.nickname + " " + self.nickname + " " + self.nickname + " :" + self.nickname
                       + " \n")
         self.irc.send("NICK " + self.nickname + "\n")
         while(True):
+	    print("looking for motd")
             temp = self.irc.recv(2048)
             if(temp == "" or temp == None or temp == "-1"):
                 break
-            if("End of /MOTD" in temp):
+            if("/MOTD" in temp):
                 break
         print("Connected to %s!" % self.server)
         return
+
+    # close irc connection 
+    def close(self):
+	self.irc.close()
+	return
 
     # join an irc channel
     # must be connected to server first
@@ -101,9 +111,6 @@ class IRC(Crawler):
         finished = False
         i = 0
 	self.connect()
-	if(channel == None):
-		self.channels = self.getChannels()
-		channel = self.channels[0]
         self.joinChannel(channel)
         print("Starting to listen... %s" % channel)
         start_time_total = time.time()
@@ -112,13 +119,15 @@ class IRC(Crawler):
         while(not finished):
             #print("cur_time: %d total_time: %d"  % ((cur_time - start_time), (cur_time - start_time_total)))
             text = self.get_text()
+	    #print("Text: %s" % text)
             if text != "":
-                print(session_text)
+                #print(session_text)
                 session_text += (text+"\n")
             cur_time = time.time()
+            #print(self.mins_interval*60)
             if((cur_time - start_time) >= self.mins_interval*60):
-                #print("="*80)
-                #print(session_text)
+                print("="*80)
+                # print(session_text)
                 utc_start_time = datetime.datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
                 utc_cur_time = datetime.datetime.fromtimestamp(cur_time).strftime('%Y-%m-%d %H:%M:%S')
                 print("Start time: %s End time: %s %s" % (utc_start_time, utc_cur_time, channel))
@@ -128,12 +137,17 @@ class IRC(Crawler):
                 start_time = time.time()
                 if((cur_time - start_time_total) >= self.total_mins*60):
                     finished = True
+		    print("finished = True")
         return
 
     # run each crawl in a thread
     def run(self, channels):
 	print(channels)
-	for i in range(0, len(channels)):
+	if len(channels) == 0:
+		self.connect()
+		channels = self.getChannels()
+		self.close()
+	for i in range(0, self.num_channels):
 	    print("running %s" % channels[i])
 	    t = threading.Thread(target=self.doCrawl, args=(channels[i], ))
 	    t.start()
@@ -141,9 +155,11 @@ class IRC(Crawler):
 
 if __name__ == "__main__":
     options = { 'mins_interval': "1",
-                'total_mins': "1",
+                'total_mins': "2",
+		'num_channels': "1",
                 # 'channels': "security,linux,information-node"
-                'channels': "linux"
+                'channels': "##linux",
+		'nickname': "tnasty11"
     }
 
     config = CrawlerConfig("irc.freenode.net", "IRC", "", "","", options)
